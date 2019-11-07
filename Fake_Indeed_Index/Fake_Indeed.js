efafcb0db4550f22363465b1a7cdc96be69343d0
legenddaniel <siyuanincanada@gmail.com>
2019-11-06 22:52:08 -0500

    Final Version

--- Fake_Indeed_Index/Fake_Indeed.js
+++ Fake_Indeed_Index/Fake_Indeed.js
@@ -1,33 +1,27 @@
 $(document).ready(() => {
     //Delete icon function
-    const deleteIcons = [$('#deleteIconWhat'), $('#deleteIconWhere')];
-    for (let deleteIcon of deleteIcons) {
-        deleteIcon.mousedown(function() {
+        $('.deleteIcon').mousedown(function() {
             $(this).prev().val('');
         })
-    }
 
     //const prev() = next() = $(this).siblings('deleteIcon'/'inputBox')
 
     //Delete icon show/hide
-    const inputBoxes = [$('#inputBoxWhat'), $('#inputBoxWhere')];
-    for (let inputBox of inputBoxes) {
-        inputBox.keyup(function() {
+        $('.inputBox').keyup(function() {
             if ($(this).val() !== '') {
                 $(this).next().show();
             } else {
                 $(this).next().hide();
             }
         });
-        inputBox.blur(function() {
+        $('.inputBox').blur(function() {
             $(this).next().hide();
         });
-        inputBox.focus(function() {
+        $('.inputBox').focus(function() {
             if ($(this).val() !== '') {
                 $(this).next().show();
             };
         });
-    }
 
     //Without issue of 'this' we can use arrow function
     $('#switchToFrench').click(() => alert('French version is provided by Indeed instead of my coding'));
