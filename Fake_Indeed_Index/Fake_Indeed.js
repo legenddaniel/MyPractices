a4a48dd158cfa90f40cfb1fbd1ad61e30add65b3
legenddaniel <siyuanincanada@gmail.com>
2019-11-07 23:12:47 -0500

    Final Version

--- Fake_Indeed_Index/Fake_Indeed.js
+++ Fake_Indeed_Index/Fake_Indeed.js
@@ -1,27 +1,27 @@
 $(document).ready(() => {
     //Delete icon function
-        $('.deleteIcon').mousedown(function() {
-            $(this).prev().val('');
-        })
+    $('.deleteIcon').mousedown(function() {
+        $(this).prev().val('');
+    })
 
     //const prev() = next() = $(this).siblings('deleteIcon'/'inputBox')
 
     //Delete icon show/hide
-        $('.inputBox').keyup(function() {
-            if ($(this).val() !== '') {
-                $(this).next().show();
-            } else {
-                $(this).next().hide();
-            }
-        });
-        $('.inputBox').blur(function() {
+    $('.inputBox').keyup(function() {
+        if ($(this).val() !== '') {
+            $(this).next().show();
+        } else {
             $(this).next().hide();
-        });
-        $('.inputBox').focus(function() {
-            if ($(this).val() !== '') {
-                $(this).next().show();
-            };
-        });
+        }
+    });
+    $('.inputBox').blur(function() {
+        $(this).next().hide();
+    });
+    $('.inputBox').focus(function() {
+        if ($(this).val() !== '') {
+            $(this).next().show();
+        };
+    });
 
     //Without issue of 'this' we can use arrow function
     $('#switchToFrench').click(() => alert('French version is provided by Indeed instead of my coding'));
