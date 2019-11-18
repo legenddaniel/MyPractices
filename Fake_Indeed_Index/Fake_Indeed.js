7af6e64ab961f46c71304c2f5028a549687cd789
legenddaniel <siyuanincanada@gmail.com>
2019-11-17 22:08:25 -0500

    Final Version

--- Fake_Indeed_Index/Fake_Indeed.js
+++ Fake_Indeed_Index/Fake_Indeed.js
@@ -1,12 +1,14 @@
+// As I mentioned in the html document, delete icon functions are not required since <input type='search'> can do the same thing
+
 $(() => {
-    //Delete icon function
+    // Delete icon function
     $('.deleteIcon').mousedown(function () {
         $(this).prev().val('');
     })
 
-    //const prev() = next() = $(this).siblings('deleteIcon'/'inputBox')
+    // const prev() = next() = $(this).siblings('deleteIcon'/'inputBox')
 
-    //Delete icon show/hide
+    // Delete icon show/hide
     $('.inputBox').keyup(function () {
         $(this).val() !== '' ? $(this).next().show() : $(this).next().hide();
     });
@@ -19,6 +21,6 @@ $(() => {
         };
     });
 
-    //Without issue of 'this' we can use arrow function, but would better unify the format (ES5/ES6)
+    // Without issue of 'this' we can use arrow function, but would better unify the format (ES5/ES6)
     $('#switchToFrench').on('click', () => alert('French version is provided by Indeed instead of my coding'));
 });
